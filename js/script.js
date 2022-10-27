7ae3c6324bb42b3b7d7b3a4f93c899ba17da701b
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-26 20:45:26 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2022 Ashley Monaghan All rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
+
+/**
+ * This function displays an alert.
+ */
+function calculate() {
+  document.getElementById("numbers").innerHTML = "<p>Hello, World!</p>"
+}
